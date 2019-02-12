756e463765fe273461730d0a2410767556f423c8
Darren Senger <darren.senger@gmail.com>
2019-02-12 07:01:27 -0800

    daily sync

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,18 @@
   <v-app class>
     <NavBar />
     <v-content>
-      <Home />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
 import NavBar from './components/NavBar'
-import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    Home
+    NavBar
   },
   data() {
     return {
--- src/components/Leases.vue
+++ src/components/Leases.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div>
+     <p>Leases</p>
+  </div>
 </template>
 
 <script>
