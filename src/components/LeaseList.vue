a07de1fe2e20fcd3c9cee798965c1f8ff4632077
Darren Senger <darren.senger@gmail.com>
2019-02-25 08:18:43 -0800

    daily sync

---
+++ src/components/LeaseList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ lseSection.num }} {{ lseSection.text }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    lseSection: Object
+  }
+}
+</script>
+
+<style scoped></style>
--- src/components/Leases.vue
+++ src/components/Leases.vue
@@ -14,7 +14,7 @@
         </v-flex>
       </v-layout>
     </v-container>-->
-    <v-btn outline block @click="runComp">button</v-btn>
+    <v-btn outline block>button</v-btn>
 
     <!-- <draggable
       :options="{ draggable: '.draggableitem' }"
@@ -108,34 +108,35 @@
       </v-expansion-panel-content> </draggable
     >-->
     <!-- ********************************************* -->
-    <draggable
+    <!-- <draggable
       :options="{ draggable: '.draggableitem' }"
-      element="v-expansion-panel"
+      element="v-treeview"
       dragoverBubble="true"
       @start="drag = true"
       @end="drag = false"
       @drop="dropHandler()"
-    >
-      <v-expansion-panel-content
+    >-->
+    <!-- <v-expansion-panel-content
         class="draggableitem"
         v-for="item in items_with_numbering"
         :key="item.id"
         width="100%"
       >
         <div v-if="item.lev === 1" slot="header">{{item.num }}{{ item.name }} </div>
-
-        <p contenteditable="false">{{ item.text }}</p>
-        <draggable
-          @start="drag = true"
-          @end="drag = false"
-        >
-          <div v-else-if"item.lev === 2">
-            <p contenteditable="false">
+          <div v-else-if="item.lev === 2"> 
               {{ item.num }}
-              {{ item.name }}
-              {{ item.text }}
-            </p>
-            <!-- <draggable
+    
+
+         </div>
+          <div v-else-if="item.lev === 3"> 
+              {{ item.num }}
+
+         </div>
+         <div v-else-if="item.lev === 4"> 
+              {{ item.num }}
+
+    </div>-->
+    <!-- <draggable
               v-model="child.children"
               @start="drag = true"
               @end="drag = false"
@@ -156,10 +157,46 @@
                   </div>
                 </draggable>
               </div>
-            </draggable> -->
+            </draggable>
           </div>
-        </draggable>
-      </v-expansion-panel-content>
+    </draggable>-->
+    <!-- </v-expansion-panel-content> -->
+    <!-- </draggable> -->
+    <draggable
+      id="dtest"
+      @start="drag = true"
+      @end="drag = false"
+      :options="{ animation: 500 }"
+      :list="dragTest"
+      v-on:change="update"
+    >
+      <div v-for="dt in dragTest" :key="dt.num" :data-id="dt.num">
+        {{ dt.num }}. {{ dt.text }}
+      </div>
+    </draggable>
+
+    <v-divider></v-divider>
+    <div style="padding-top: 30px"></div>
+
+    <draggable
+      @start="drag = true"
+      @end="drag = false"
+      :options="{ animation: 500 }"
+      :list="lseSection_with_numbering"
+    >
+      <!-- <div
+        id="lse"
+        v-for="item in items_with_numbering"
+        :key="item.order"
+        :data-id="item.order"
+      >-->
+      <LeaseList
+        v-for="lseSection in lseSection_with_numbering"
+        :key="lseSection.id"
+        :lseSection="lseSection"
+      ></LeaseList>
+
+      <!-- </div> -->
     </draggable>
   </div>
 </template>
@@ -167,45 +204,136 @@
 <script>
 import EventService from '@/services/EventService.js'
 import draggable from 'vuedraggable'
+import LeaseList from '@/components/LeaseList.vue'
+// import Sortable from 'sortable'
 
 export default {
   components: {
-    draggable
+    draggable,
+    LeaseList
   },
+
   data() {
     return {
       event: null,
       events: [],
-      itemLevel1: 1,
-      itemLevel2: 0,
-      itemLevel3: 0,
-      // create an array for rendering, a la https://codepen.io/anon/pen/ZwVJrg?editors=1010
+
+      i: 0,
+      j: -1,
+      k: -1,
+      l: -1,
+      dragTest: [
+        { text: 'item 1', num: '1' },
+        { text: 'item 2', num: '2' },
+        { text: 'item 3', num: '3' },
+        { text: 'item 4', num: '4' },
+        { text: 'item 5', num: '5' }
+      ],
+      // create an array for rendering, a la https://codepen.io/anon/pen/ZwVJrg?editors=1010,
+      items2: [
+        {
+          id: 1,
+          name: 'Applications :',
+          children: [
+            { id: 2, name: 'Calendar : app' },
+            { id: 3, name: 'Chrome : app' },
+            { id: 4, name: 'Webstorm : app' }
+          ]
+        },
+        {
+          id: 5,
+          name: 'Documents :',
+          children: [
+            {
+              id: 6,
+              name: 'vuetify :',
+              children: [
+                {
+                  id: 7,
+                  name: 'src :',
+                  children: [
+                    { id: 8, name: 'index : ts' },
+                    { id: 9, name: 'bootstrap : ts' }
+                  ]
+                }
+              ]
+            },
+            {
+              id: 10,
+              name: 'material2 :',
+              children: [
+                {
+                  id: 11,
+                  name: 'src :',
+                  children: [
+                    { id: 12, name: 'v-btn : ts' },
+                    { id: 13, name: 'v-card : ts' },
+                    { id: 14, name: 'v-window : ts' }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 15,
+          name: 'Downloads :',
+          children: [
+            { id: 16, name: 'October : pdf' },
+            { id: 17, name: 'November : pdf' },
+            { id: 18, name: 'Tutorial : html' }
+          ]
+        },
+        {
+          id: 19,
+          name: 'Videos :',
+          children: [
+            {
+              id: 20,
+              name: 'Tutorials :',
+              children: [
+                { id: 21, name: 'Basic layouts : mp4' },
+                { id: 22, name: 'Advanced techniques : mp4' },
+                { id: 23, name: 'All about app : dir' }
+              ]
+            },
+            { id: 24, name: 'Intro : mov' },
+            { id: 25, name: 'Conference introduction : avi' }
+          ]
+        }
+      ],
       items: [
         {
           id: 1,
-          num: null,
+          num: '1.0',
+          order: 1,
           name: 'Applications :',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          text: 'Lorem ipsum 1',
+          lev: 1,
           children: [
             {
               id: 2,
-              num: null,
+              num: '1.1',
+              order: 2,
               name: 'Calendar',
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+              text: 'Lorem ipsum 2',
+              lev: 2,
               children: [
                 {
                   id: 10,
-                  num: null,
+                  num: '1.1.1',
+                  order: 3,
                   name: 'rsaa',
-                  text:
-                    'Lorem ipsum dolor adjsasjiopewo sit amet, consectetur adipiscing elit',
+                  text: 'Lorem ipsum 3',
+                  lev: 3,
                   children: [
                     {
                       id: 11,
-                      num: null,
+                      num: '1.1.1.1',
+                      order: 4,
                       name: 'gggaa',
-                      text:
-                        'jlejoiqwjeiojqwiopejopieqwiot, consectetur adipiscing elit'
+                      text: 'Lorem 4',
+                      lev: 4
                     }
                   ]
                 }
@@ -213,50 +341,61 @@ export default {
             },
             {
               id: 3,
-              num: null,
+              num: '1.2',
+              order: 5,
               name: 'Chrome',
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+              text: 'Lorem ipsum 5',
+              lev: 2
             },
             {
               id: 4,
-              num: null,
+              num: '1.3',
+              order: 6,
               name: 'Webstorm',
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+              text: 'Lorem ipsum 6',
+              lev: 2
             }
           ]
         },
         {
           id: 5,
-          num: null,
+          num: '2.0',
+          order: 7,
           name: 'Documents :',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          text: 'Lorem ipsum 7',
+          lev: 1,
           children: [
             {
               id: 6,
-              num: null,
+              num: '2.1',
+              order: 8,
               name: 'vuetify : ',
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+              text: 'Lorem ipsum 8',
+              lev: 2,
               children: [
                 {
                   id: 7,
-                  num: null,
+                  num: '2.1.1',
+                  order: 9,
                   name: 'src :',
-                  text:
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                  text: 'Lorem ipsum 9',
+                  lev: 3,
                   children: [
                     {
                       id: 8,
-                      num: null,
+                      num: '2.1.1.1',
+                      order: 10,
                       name: 'index : ts :',
-                      text:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                      text: 'Lorem ipsum 10',
+                      lev: 4
                     },
                     {
                       id: 9,
-                      num: null,
+                      num: '2.1.1.2',
+                      order: 11,
                       name: 'bootstrap : ts : ',
-                      text:
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                      text: 'Lorem ipsum 11',
+                      lev: 4
                     }
                   ]
                 }
@@ -268,27 +407,61 @@ export default {
     }
   },
   methods: {
+    incri() {
+      alert('incri')
+      this.i++
+    },
     renumber(ls) {
       console.log('renumber me' + ls.id)
     },
-    dropHandler() {
+    dropHandler(ev) {
       alert('dropHandler')
-      console.log('dropHandler')
+      alert(ev)
+      console.log(this.dragTest)
     },
-    runComp() {
+    onMoveCallback(evt, originalEvent) {
+      alert(evt)
+      alert(originalEvent)
+    },
+    update() {
+      this.dragTest.map((item, index) => {
+        item.num = index + 1
+      })
+
+      for (let i = 0; i < this.dragTest.length; i++) {
+        console.log(this.dragTest[i].num + ': ' + this.dragTest[i].text)
+      }
+    },
+    updateComplex() {
       console.log(this.items_with_numbering)
-      // alert('runComps')
+      this.items_with_numbering.map((item, index) => {
+        item.order = index + 1
+      })
+      console.log(this.items_with_numbering)
     }
+    // dropTest(evt) {
+    //   console.log(evt.draggedContext.index)
+    //   var el = document.getElementById('dtest')
+    //   var children = el.getElementsByTagName('div')
+    //   var arChildren = Array.from(children)
+    //   for (let i = 0; i < arChildren.length; i++) {
+    //     arChildren[i].num = i + 1
+    //     console.log('Before reset: ' + this.dragTest[i].num)
+    //     this.dragTest[i].num = i + 1
+    //     console.log('After reset: ' + this.dragTest[i].num)
+    //   }
+    // }
   },
 
   computed: {
     // eslint-disable-next-line vue/return-in-computed-property
-    items_with_numbering() {
+    lseSection_with_numbering() {
       const result = []
       let lev1 = 0
       let lev2 = 0
       let lev3 = 0
       let lev4 = 0
+      let order = 0
 
       let objI
       let itemI
@@ -298,11 +471,13 @@ export default {
         // if (item.children) {
         // }
         lev1 += 1
+
         objI = {
           id: this.items[i].id,
           name: this.items[i].name,
           text: this.items[i].text,
           lev: 1,
+          order: order++,
           num: lev1 + '.0'
         }
         result.push(objI)
@@ -319,6 +494,7 @@ export default {
               name: this.items[i].children[j].name,
               text: this.items[i].children[j].text,
               lev: 2,
+              order: order++,
               num: lev1 + '.' + lev2
             }
             // console.log(objI)
@@ -339,6 +515,7 @@ export default {
                   name: this.items[i].children[j].children[k].name,
                   text: this.items[i].children[j].children[k].text,
                   lev: 3,
+                  order: order++,
                   num: lev1 + '.' + lev2 + '.' + lev3
                 }
                 // console.log(objI)
@@ -365,6 +542,7 @@ export default {
                       text: this.items[i].children[j].children[k].children[l]
                         .text,
                       lev: 4,
+                      order: order++,
                       num: lev1 + '.' + lev2 + '.' + lev3 + '.' + lev4
                     }
                     // console.log(objI)
